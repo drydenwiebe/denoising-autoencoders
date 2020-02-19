373ae5a2960f8da28b5b9339446ae81d57756076
Dryden Wiebe <dryden.wiebe@live.com>
2020-02-19 12:27:21 -0800

    Update conv_dae

--- conv_dae.py
+++ conv_dae.py
@@ -102,76 +102,80 @@ class ConvolutionalDenoiser(nn.Module):
         x = torch.sigmoid(self.conv_out(x))
                 
         return x
-    
-# initalize the neural network
-model = ConvolutionalDenoiser()
-print(model)
 
-# specify loss function
-criterion = nn.MSELoss()
+def run_denoiser():
+    # initalize the neural network
+    model = ConvolutionalDenoiser()
+    print(model)
 
-# specify loss function
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    # specify loss function
+    criterion = nn.MSELoss()
 
-for epoch in range(1, n_epochs+1):
-    # monitor training loss
-    train_loss = 0.0
-    
-    ###################
-    # train the model #
-    ###################
-    for data in train_loader:
-        # _ stands in for labels, here
-        # no need to flatten images
-        images, _ = data
+    # specify loss function
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(1, n_epochs+1):
+        # monitor training loss
+        train_loss = 0.0
         
-        ## add random noise to the input images
-        noisy_imgs = images + noise_factor * torch.randn(*images.shape)
-        # Clip the images to be between 0 and 1
-        noisy_imgs = np.clip(noisy_imgs, 0., 1.)
-                
-        # clear the gradients of all optimized variables
-        optimizer.zero_grad()
-        ## forward pass: compute predicted outputs by passing *noisy* images to the model
-        outputs = model(noisy_imgs)
-        # calculate the loss
-        # the "target" is still the original, not-noisy images
-        loss = criterion(outputs, images)
-        # backward pass: compute gradient of the loss with respect to model parameters
-        loss.backward()
-        # perform a single optimization step (parameter update)
-        optimizer.step()
-        # update running training loss
-        train_loss += loss.item()*images.size(0)
+        ###################
+        # train the model #
+        ###################
+        for data in train_loader:
+            # _ stands in for labels, here
+            # no need to flatten images
+            images, _ = data
             
-    # print avg training statistics 
-    train_loss = train_loss/len(train_loader)
-    print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
+            ## add random noise to the input images
+            noisy_imgs = images + noise_factor * torch.randn(*images.shape)
+            # Clip the images to be between 0 and 1
+            noisy_imgs = np.clip(noisy_imgs, 0., 1.)
+                    
+            # clear the gradients of all optimized variables
+            optimizer.zero_grad()
+            ## forward pass: compute predicted outputs by passing *noisy* images to the model
+            outputs = model(noisy_imgs)
+            # calculate the loss
+            # the "target" is still the original, not-noisy images
+            loss = criterion(outputs, images)
+            # backward pass: compute gradient of the loss with respect to model parameters
+            loss.backward()
+            # perform a single optimization step (parameter update)
+            optimizer.step()
+            # update running training loss
+            train_loss += loss.item()*images.size(0)
+                
+        # print avg training statistics 
+        train_loss = train_loss/len(train_loader)
+        print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
 
-# obtain one batch of test images
-dataiter = iter(test_loader)
-images, labels = dataiter.next()
+    # obtain one batch of test images
+    dataiter = iter(test_loader)
+    images, labels = dataiter.next()
 
-# add noise to the test images
-noisy_imgs = images + noise_factor * torch.randn(*images.shape)
-noisy_imgs = np.clip(noisy_imgs, 0., 1.)
+    # add noise to the test images
+    noisy_imgs = images + noise_factor * torch.randn(*images.shape)
+    noisy_imgs = np.clip(noisy_imgs, 0., 1.)
 
-# get sample outputs
-output = model(noisy_imgs)
-# prep images for display
-noisy_imgs = noisy_imgs.numpy()
+    # get sample outputs
+    output = model(noisy_imgs)
+    # prep images for display
+    noisy_imgs = noisy_imgs.numpy()
 
-# output is resized into a batch of iages
-output = output.view(batch_size, 1, 28, 28)
-# use detach when it's an output that requires_grad
-output = output.detach().numpy()
+    # output is resized into a batch of iages
+    output = output.view(batch_size, 1, 28, 28)
+    # use detach when it's an output that requires_grad
+    output = output.detach().numpy()
 
-# plot the first ten input images and then reconstructed images
-fig, axes = plt.subplots(nrows=2, ncols=10, sharex=True, sharey=True, figsize=(25,4))
+    # plot the first ten input images and then reconstructed images
+    fig, axes = plt.subplots(nrows=2, ncols=10, sharex=True, sharey=True, figsize=(25,4))
 
-# input images on top row, reconstructions on bottom
-for noisy_imgs, row in zip([noisy_imgs, output], axes):
-    for img, ax in zip(noisy_imgs, row):
-        ax.imshow(np.squeeze(img), cmap='gray')
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
+    # input images on top row, reconstructions on bottom
+    for noisy_imgs, row in zip([noisy_imgs, output], axes):
+        for img, ax in zip(noisy_imgs, row):
+            ax.imshow(np.squeeze(img), cmap='gray')
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+
+if __name__ == "__main__":
+    run_denoiser()
